2c37004e077e63169ca68265742ee4c18ee6f247
esadeymen <esadeymen@gmail.com>
2021-04-04 20:44:16 +0300

    version 02

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -4,8 +4,8 @@ public class MyGitClass {
 
 	public static void main(String[] args) {
 
-
-		System.out.println("version 1");
+        System.out.println("version 1");
+        System.out.println("my second commit");
 
 	}
 
